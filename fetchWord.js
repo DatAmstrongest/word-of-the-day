4baf46abf366b316ca5eaf662916650864f7c8b8
DatAmstrongest <93247554+DatAmstrongest@users.noreply.github.com>
2026-04-05 22:54:54 +0300

    Writable boxes are added

--- fetchWord.js
+++ fetchWord.js
@@ -2,6 +2,11 @@
 const changeBackground = () => {
   document.body.style.backgroundImage = "url('./assets/background/background-" + Math.ceil(Math.random() * 9) + ".jpg')";
 }
+
+const capitalizeFirstLetter = (val) => {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
 const getWord = () => {
   fetch("https://www.merriam-webster.com/word-of-the-day", {
     mode: "cors", // Ensures CORS is attempted (though Google may block it)
@@ -17,14 +22,51 @@ const getWord = () => {
     .then(data => {
         const parser = new DOMParser();
         const doc3 = parser.parseFromString(data, "text/html");
-        document.getElementById("word-title").innerHTML = doc3.getElementsByClassName("word-header-txt")[0].textContent
-        document.getElementById("word-description").innerHTML = doc3.querySelector(".wod-definition-container p").textContent
-        console.log("Response data:", data);  // Logging the response text
+        const word = doc3.getElementsByClassName("word-header-txt")[0].textContent
+        const definition = doc3.querySelector(".wod-definition-container p").textContent
+        const changed_definition = definition.replace(capitalizeFirstLetter(word), "This word");
+        //document.getElementById("word-title").innerHTML = 
+        document.getElementById("word-description").innerHTML = changed_definition;
+        createGame(word);
     })
     .catch(error => {
       console.error("Error:", error);
     });
 }
 
+const createGame = (word) => {
+ const word_length =  word.length
+ const html_container = document.getElementsByClassName("container").item(0);
+ for (var i=0; i<word_length; i++){
+    html_box = document.createElement("div");
+    html_box.classList.add("box")
+    html_box.setAttribute("id", "box"+i);
+    html_container.appendChild(html_box);
+ }
+
+}
+
+
+
 changeBackground();
 getWord();
+var currentIndex = 0;
+document.addEventListener("keydown", function (event) {
+    if (event.key == "Backspace"){
+      if (currentIndex != 0){
+          currentIndex -= 1
+          html_box = document.getElementById("box"+currentIndex);
+          html_box.textContent = "";
+      }
+    }
+    else if (event.key == "Enter"){
+      return;
+    }
+    else{
+      html_box = document.getElementById("box"+currentIndex);
+      if (html_box != undefined){
+        html_box.textContent = event.key;
+        currentIndex += 1;
+      }
+    }
+});
